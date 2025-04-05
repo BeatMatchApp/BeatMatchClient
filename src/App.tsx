71076a81b8629964bbf8a0e9c124f3bab1f7427a
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-04-05 19:04:30 +0300

    fix UserDetails css

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
 import { useState } from "react";
-import styled from "@emotion/styled";
-import { Button, Paper, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import UserDetails from "./components/userDetails/userDetails";
 import "./App.css";
-import { getGeminiAnswer } from "./services/geminiService";
-
-const StyledMenuButton = styled(Button)({
-  backgroundColor: "#715cf8",
-  color: "white",
-  width: "12em",
-  "&:hover": {
-    backgroundColor: "#5a4cc8",
-  },
-});
+import { StyledMenuButton } from "./components/styledComponents";
 
 function App() {
   const [isUserDetailsOpen, setIsUserDetailsOpen] = useState(false);
-  const [mood, setMood] = useState("");
-  const [favoriteArtist, setFavoriteArtist] = useState("");
-  const [suggestion, setSuggestion] = useState("");
-
-  const handleGetSuggestion = async () => {
-    try {
-      const response = await getGeminiAnswer(favoriteArtist, mood);
-      setSuggestion(response.suggestion);
-    } catch (error) {
-      console.error("Error fetching suggestion:", error);
-      alert("Failed to fetch suggestion. Please try again.");
-    }
-  };
 
   return (
     <>
       {!isUserDetailsOpen && (
-        <Paper className="MenuCard">       
+        <Box className="MenuCard">       
         <img width="300em" src={`/assets/logo.png`} loading="lazy" className="logoImg" />
           <StyledMenuButton onClick={() => setIsUserDetailsOpen(true)}>Login to spotify</StyledMenuButton>
-          <TextField id="mood" label="mood" onChange={(e) => setMood(e.target.value)}/>
-          <TextField id="favoriteArtist" label="favorite artist" onChange={(e) => setFavoriteArtist(e.target.value)}/>
-        <StyledMenuButton onClick={handleGetSuggestion}>Get suggestion</StyledMenuButton>
-        {suggestion && <Typography> {`The suggestion is ${suggestion}`}</Typography> }
-        </Paper>
+        </Box>
       )}
       {isUserDetailsOpen && <UserDetails />}
     </>
--- src/components/userDetails/userDetails.tsx
+++ src/components/userDetails/userDetails.tsx
@@ -9,6 +9,9 @@ import {
 } from "../../services/spotifyService";
 import { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
+import { Box, CircularProgress, TextField, Typography } from "@mui/material";
+import { StyledMenuButton } from "../styledComponents";
+import { getGeminiAnswer } from "../../services/geminiService";
 
 function UserDetails() {
   const [user, setUser] = useState<UserSpotifyProfile | null>(null);
@@ -17,6 +20,9 @@ function UserDetails() {
   const [playlistName, setPlaylistName] = useState("");
   const [songName, setSongName] = useState("");
   const [artistName, setArtistName] = useState("");
+  const [mood, setMood] = useState("");
+  const [favoriteArtist, setFavoriteArtist] = useState("");
+  const [suggestion, setSuggestion] = useState("");
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -50,7 +56,7 @@ function UserDetails() {
         user!.id
       );
 
-      if (playlistDetails.success) {
+      if (playlistDetails.id) {
         setPlaylistId(playlistDetails.id);
         toast("Playlist created successfully");
       } else {
@@ -80,52 +86,48 @@ function UserDetails() {
     }
   };
 
-  if (!user) return <p>Loading...</p>;
+  const handleGetSuggestion = async () => {
+    try {
+      const response = await getGeminiAnswer(favoriteArtist, mood);
+      setSuggestion(response.suggestion);
+    } catch (error) {
+      console.error("Error fetching suggestion:", error);
+      alert("Failed to fetch suggestion. Please try again.");
+    }
+  };
+
+  if (!user) 
+    return (
+    <Box sx={{ display: 'flex' }}>
+      <CircularProgress size="5em" color="secondary"/>
+    </Box>
+    );
 
   return (
-    <div className="user-details">
-      <h1>BeatMatch</h1>
+    <Box className="MenuCard">       
+        <img width="300em" src={`/assets/logo.png`} loading="lazy" className="logoImg" />
 
       {!playlistId && (
-        <div>
-          <section id="profile">
-            <h2>
-              Logged in as <span>{user.display_name}</span>
-            </h2>
-          </section>
-          <input
-            type="text"
-            placeholder="Playlist name"
-            value={playlistName}
-            onChange={(e) => setPlaylistName(e.target.value)}
-            className="song-input"
-          />
-          <button onClick={createPlaylistInSpotify}>Create Playlist</button>
-        </div>
+        <>
+          <Typography sx={{ color: '#715cf8', fontWeight: 'bold'}}> Logged in as {user.display_name} </Typography>
+          <TextField id="playlistName" label="Playlist name" onChange={(e) => setPlaylistName(e.target.value)}/>
+          <StyledMenuButton onClick={createPlaylistInSpotify}>Create Playlist</StyledMenuButton>
+
+          <TextField id="mood" label="mood" onChange={(e) => setMood(e.target.value)}/>
+          <TextField id="favoriteArtist" label="favorite artist" onChange={(e) => setFavoriteArtist(e.target.value)}/>
+          <StyledMenuButton onClick={handleGetSuggestion}>Get suggestion</StyledMenuButton>
+          {suggestion && <Typography sx={{ color: '#715cf8', fontWeight: 'bold'}}> {`The suggestion is ${suggestion}`}</Typography> }
+          </>
       )}
       {playlistId && (
-        <div className="song-form">
-          <input
-            type="text"
-            placeholder="Song name"
-            value={songName}
-            onChange={(e) => setSongName(e.target.value)}
-            className="song-input"
-          />
-          <input
-            type="text"
-            placeholder="Artist name"
-            value={artistName}
-            onChange={(e) => setArtistName(e.target.value)}
-            className="song-input"
-          />
-          <button onClick={addSongToSpotifyPlaylist} className="song-button">
-            Add Song
-          </button>
-        </div>
+        <>
+          <TextField id="songInput" label="Song name" onChange={(e) => setSongName(e.target.value)}/>
+          <TextField id="artistInput" label="Artist name" onChange={(e) => setArtistName(e.target.value)}/>
+          <StyledMenuButton onClick={addSongToSpotifyPlaylist}>Add Song</StyledMenuButton>
+        </>
       )}
       <ToastContainer />
-    </div>
+    </Box>
   );
 }
 

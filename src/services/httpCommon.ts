f0fdfdffdcb8402142d8d0952def3ec4f2cd2039
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-04-27 01:31:20 +0300

    add-auth

--- src/services/httpCommon.ts
+++ src/services/httpCommon.ts
@@ -16,3 +16,34 @@ export const serverService = axios.create({
   },
   withCredentials: true,
 });
+
+const refreshTokenService = axios.create({
+  baseURL: envConfig.BACKEND_SERVICE_URL,
+  withCredentials: true,
+});
+
+serverService.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/login")
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        await refreshTokenService.get("/auth/refresh");
+        
+        return await axios(originalRequest);
+      } catch (refreshError) {
+        window.location.href = "/login";
+        console.log("Error: ", refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);

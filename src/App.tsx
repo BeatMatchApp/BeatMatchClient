ea00715cb41507a84eb1550f4a9db9150af0fc8b
ayatal <ayatal.712@cs.colman.ac.il>
2025-05-11 22:03:47 +0300

    common styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,37 @@ import RegisterSpotifyPage from "./pages/RegisterSpotifyPage/RegisterSpotifyPage
 import { NavigationRoutes } from "./models/NavigationRoutes";
 import { ToastContainer } from "react-toastify";
 import ProfileForm from "./pages/ProfileForm/profileForm";
+import { ThemeProvider } from "@mui/material";
+import theme from "./styles/consts";
 
 function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Provider store={store}>
-        <Router>
-          <>
-            <Routes>
-              <Route path={NavigationRoutes.HOME} element={<HomePage />} />
-              <Route path={NavigationRoutes.LOGIN} element={<LoginPage />} />
-              <Route
-                path={NavigationRoutes.REGISTER_SPOTIFY}
-                element={<RegisterSpotifyPage />}
-              />
-              <Route
-                path={NavigationRoutes.USER_ACTIONS_PAGE}
-                element={<UserDetails />}
-              />
-              <Route
-                path={NavigationRoutes.PROFILE_FORM}
-                element={<ProfileForm />}
-              />
-            </Routes>
-            <ToastContainer />
-          </>
-        </Router>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
+            <>
+              <Routes>
+                <Route path={NavigationRoutes.HOME} element={<HomePage />} />
+                <Route path={NavigationRoutes.LOGIN} element={<LoginPage />} />
+                <Route
+                  path={NavigationRoutes.REGISTER_SPOTIFY}
+                  element={<RegisterSpotifyPage />}
+                />
+                <Route
+                  path={NavigationRoutes.USER_ACTIONS_PAGE}
+                  element={<UserDetails />}
+                />
+                <Route
+                  path={NavigationRoutes.PROFILE_FORM}
+                  element={<ProfileForm />}
+                />
+              </Routes>
+              <ToastContainer />
+            </>
+          </Router>
+        </Provider>
+      </ThemeProvider>
     </LocalizationProvider>
   );
 }

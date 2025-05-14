03fd0fa826aa6ef93ecd684f7e59bfefd7f01039
CarmelBarda <carmelbd78@colman.ac.il>
2025-05-14 19:42:35 +0300

    move to server

--- src/components/createPlaylist/createPlaylist.tsx
+++ src/components/createPlaylist/createPlaylist.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { ScrollableSelector } from "../scrollableSelector/scrollableSelector";
 import {
   Wrapper,
@@ -9,13 +9,40 @@ import {
   ContinueButton,
   Title,
 } from "./styled";
-import { EVENT_CATEGORIES, MOOD_CATEGORIES } from "./consts";
+import { getEvents, getMoods } from "../../services/metsService";
 
 export const CreatePlaylist = () => {
   const [playlistName, setPlaylistName] = useState("");
   const [event, setEvent] = useState<string | null>(null);
   const [mood, setMood] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [events, setEvents] = useState<string[]>([]);
+  const [moods, setMoods] = useState<string[]>([]);
+
+  const fetchEvents = async () => {
+    try {
+      const response = await getEvents();
+      setEvents(response);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  const fetchMoods = async () => {
+    try {
+      const response = await getMoods();
+      setMoods(response);
+    } catch (error) {
+      console.error("Error fetching moods:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+    fetchMoods();
+  }, []);
+
+  fetchEvents();
 
   const validatePlaylistName = (name: string): boolean => {
     const isValid = name.trim().length > 0 && name.length <= 100;
@@ -62,14 +89,14 @@ export const CreatePlaylist = () => {
 
         <ScrollableSelector
           title="Special event?"
-          items={EVENT_CATEGORIES}
+          items={events}
           selected={event}
           onSelect={setEvent}
         />
 
         <ScrollableSelector
           title="Specific mood?"
-          items={MOOD_CATEGORIES}
+          items={moods}
           selected={mood}
           onSelect={setMood}
         />

b2e774e988647ea0f08cb446ad8ccd0c959705e8
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-06-07 11:57:20 +0300

    url page

--- src/components/createPlaylistFinish/createPlaylistFinish.tsx
+++ src/components/createPlaylistFinish/createPlaylistFinish.tsx
@@ -1,3 +1,4 @@
+import ShinyCard from "../ShinyCard/ShinyCard";
 import { StyledContentContainer, StyledPageTitle } from "../styledComponents";
 import { Box, Typography } from "@mui/material";
 
@@ -10,10 +11,10 @@ export const CreatePlaylistFinish: React.FC<Props> = ({ PlaylistUrl }) => {
   return (
      <Box className="center">
        <StyledPageTitle>Playlist created!</StyledPageTitle>
-       <StyledContentContainer sx={{width: '80%', color: 'white'}}>
-        <Box sx={{ backgroundColor: '#9C27B0', }}>
-            <Typography sx={{ padding: '10px'}}>{PlaylistUrl}</Typography>
-        </Box>
+       <StyledContentContainer sx={{ color: 'white', alignItems: 'center' }}>
+        <ShinyCard colors={['#8d92f6', '#a2dfd0']}>
+          <Typography sx={{ padding: '1vh'}}>{PlaylistUrl}</Typography>
+        </ShinyCard>
        </StyledContentContainer>
      </Box>
   );

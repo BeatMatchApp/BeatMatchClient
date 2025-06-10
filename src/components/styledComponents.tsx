024268bb13b81886e9298f6c140a05673f985fc4
ayatal <ayatal.712@cs.colman.ac.il>
2025-06-10 17:24:12 +0300

    use background inly in app css

--- src/components/styledComponents.tsx
+++ src/components/styledComponents.tsx
@@ -369,9 +369,4 @@ export const StyledMainBox = styled(Box)(() => ({
   height: '100vh',
   overflow: 'hidden',
   position: 'relative',
-  backgroundImage: `url('/assets/images/main-background.jpg')`,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  backgroundAttachment: 'fixed',
 }));

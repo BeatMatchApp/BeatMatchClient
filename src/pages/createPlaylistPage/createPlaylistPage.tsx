80ea33df541857f0fb61cd928b3b29071982f229
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-06-11 18:16:31 +0300

    fix-mood-and-event-fields

--- src/pages/createPlaylistPage/createPlaylistPage.tsx
+++ src/pages/createPlaylistPage/createPlaylistPage.tsx
@@ -89,13 +89,6 @@ const CreatePlaylistPage: React.FC = () => {
   };
 
   const savePlaylist = async () => {
-    if (!mood || !event) {
-      setSavedPlaylist({
-        error: true,
-        errorMessage: 'Mood and event must be selected to create a playlist.',
-      });
-      return;
-    }
     setLoading(true);
     try {
       const formattedSongs = songs.map((song) => ({

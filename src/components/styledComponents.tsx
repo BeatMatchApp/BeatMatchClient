5679c8bc954fef900da770fbe51f493274330b8d
CarmelBarda <carmelbd78@colman.ac.il>
2025-06-11 13:05:55 +0300

    fix background

--- src/components/styledComponents.tsx
+++ src/components/styledComponents.tsx
@@ -369,7 +369,7 @@ export const StyledMainBox = styled(Box)(() => ({
   height: '100vh',
   overflow: 'hidden',
   position: 'relative',
-  backgroundImage: `url('/assets/images/main-background.jpg')`,
+  backgroundImage: `url('../../public/assets/images/main-background.jpg')`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',

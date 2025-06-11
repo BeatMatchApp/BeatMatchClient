7aab9bc79011c9aad8a2346ec92badd4de2d8613
ayatal <ayatal.712@cs.colman.ac.il>
2025-06-11 15:02:37 +0300

    add tooltip

--- src/components/moodBoard/moodBoard.tsx
+++ src/components/moodBoard/moodBoard.tsx
@@ -6,6 +6,7 @@ import {
   YAxis,
   ResponsiveContainer,
   Cell,
+  Tooltip,
 } from 'recharts';
 import { Card, CardContent } from '@mui/material';
 import { Playlist } from '../../models/Playlist';
@@ -28,8 +29,9 @@ const MoodPlaylistsChart: React.FC<Props> = ({ playlists }) => {
     mood,
     count,
   }));
+
   return (
-    <Card className="p-4 shadow-xl rounded-2xl">
+    <Card className="p-4 shadow-xl rounded-2xl" sx={{ bgcolor: '#f2feff' }}>
       <CardContent>
         <StyledPageSubtitle>Welcome to your mood board!</StyledPageSubtitle>
         <ResponsiveContainer width="100%" height={300}>
@@ -37,9 +39,23 @@ const MoodPlaylistsChart: React.FC<Props> = ({ playlists }) => {
             data={data}
             margin={{ top: 20, right: 30, left: 0, bottom: 5 }}
           >
-            <XAxis dataKey="mood" style={{ fontSize: 14 }} />
+            <XAxis dataKey="mood" style={{ fontSize: 10 }} />
             <YAxis allowDecimals={false} style={{ fontSize: 14 }} />
-            <Bar dataKey="count" radius={[10, 10, 0, 0]}>
+            <Tooltip
+              cursor={{ fill: 'transparent' }}
+              contentStyle={{
+                backgroundColor: '#fcfcf7',
+                borderRadius: 6,
+                fontSize: 10,
+                padding: '3px 10px',
+                boxShadow: '0px 2px 6px rgba(0,0,0,0.1)',
+              }}
+            />
+            <Bar
+              dataKey="count"
+              radius={[10, 10, 0, 0]}
+              isAnimationActive={false}
+            >
               {data.map((entry, index) => (
                 <Cell
                   key={`cell-${index}`}

777d86671691f8b4d7dcc7748e46847e5b8c8669
ayatal <ayatal.712@cs.colman.ac.il>
2025-06-14 23:26:28 +0300

    try screen height fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,40 +12,61 @@ import ProfileForm from './pages/ProfileForm/profileForm';
 import { ThemeProvider } from '@mui/material';
 import theme from './styles/consts';
 import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const setVisibleScreenHeight = () => {
+      const vh: number = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setVisibleScreenHeight();
+
+    window.addEventListener('resize', setVisibleScreenHeight);
+
+    return () => {
+      window.removeEventListener('resize', setVisibleScreenHeight);
+    };
+  }, []);
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <Router>
-            <>
-              <Routes>
-                <Route path={NavigationRoutes.HOME} element={<MainPage />} />
-                <Route path={NavigationRoutes.LOGIN} element={<LoginPage />} />
-                <Route
-                  path={NavigationRoutes.REGISTER_SPOTIFY}
-                  element={<RegisterSpotifyPage />}
-                />
-                <Route
-                  path={NavigationRoutes.PROFILE_FORM}
-                  element={<ProfileForm />}
-                />
-                <Route
-                  path={NavigationRoutes.MAIN_PAGE + '/*'}
-                  element={<MainPage />}
-                />
-                <Route
-                  path="*"
-                  element={<Navigate to={NavigationRoutes.HOME} replace />}
-                />
-              </Routes>
-              <ToastContainer position="bottom-center" />
-            </>
-          </Router>
-        </Provider>
-      </ThemeProvider>
-    </LocalizationProvider>
+    <div className="full-page">
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <Router>
+              <>
+                <Routes>
+                  <Route path={NavigationRoutes.HOME} element={<MainPage />} />
+                  <Route
+                    path={NavigationRoutes.LOGIN}
+                    element={<LoginPage />}
+                  />
+                  <Route
+                    path={NavigationRoutes.REGISTER_SPOTIFY}
+                    element={<RegisterSpotifyPage />}
+                  />
+                  <Route
+                    path={NavigationRoutes.PROFILE_FORM}
+                    element={<ProfileForm />}
+                  />
+                  <Route
+                    path={NavigationRoutes.MAIN_PAGE + '/*'}
+                    element={<MainPage />}
+                  />
+                  <Route
+                    path="*"
+                    element={<Navigate to={NavigationRoutes.HOME} replace />}
+                  />
+                </Routes>
+                <ToastContainer position="bottom-center" />
+              </>
+            </Router>
+          </Provider>
+        </ThemeProvider>
+      </LocalizationProvider>
+    </div>
   );
 }
 
